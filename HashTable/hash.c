9a9f4d6921af0cffc62fae9a3ad84888fb1dc899
Braiden <bmxfiend1@gmail.com>
2021-01-19 09:03:51 -0500

    Fixed if statement style

--- HashTable/hash.c
+++ HashTable/hash.c
@@ -18,13 +18,9 @@ void clearNewLineDelimiter(char* psInputWithNewline);
 
 int main(void)
 {
-    // Declare the hash table.
     struct NamesOfPeople* arspHashTable[KTABLELIMIT];
-
-    // Initialize every position of the hash table.
     initializeHashTable(arspHashTable);
 
-    // Gather the hash keys to pass into the insert function.
     int iHashValue = 0;
     int iReturnValue = 0;
     char arsBuffer[K50BYTES] = {""};
@@ -43,10 +39,12 @@ int main(void)
         iReturnValue = iInsertPeoplesNames(arspHashTable, arsBuffer, iHashValue);
 
         // If we ran out of system resources.
-        if (iReturnValue == 2) { return 2; }
+        if (iReturnValue == 2) 
+        { 
+            return 2;
+        }
     } while (iReturnValue != 1);
 
-    // Display peoples names to the console window.
     showTheTableOfNames(arspHashTable);
 
     // Prompt the user for a name to lookup.
@@ -54,31 +52,28 @@ int main(void)
     printf(KPROMPT_FOR_LOOKUPNAME);
     fgets(arsLookupName, K50BYTES, stdin);
 
-    // Send the name to lookup to the search function.
     if ((iHashTableLookup(arspHashTable, arsLookupName)) == 1)
     {
         printf("\n\t[ The name was not found in the system. ]\n");
     }
-    else { printf("\n\t[ The name exists ! ]\n"); }
+    else 
+    { 
+        printf("\n\t[ The name exists ! ]\n");
+    }
 
-    // Prompt the user for a name to delete.
     char arsNameToDelete[K50BYTES] = {""};
     printf(KPROMPT_FOR_NAMETODELETE);
     fgets(arsNameToDelete, K50BYTES, stdin);
 
-    // Send the name to delete to the delete function.
     if ((iDeleteAPersonsName(arspHashTable, arsNameToDelete)) == 1)
     {
         printf("\n\t[ The name does not exist ! ]\n");
     }
     else 
     {   
-        // Display the table one last time after a name has been deleted.
         showTheTableOfNames(arspHashTable);
         printf("\n\t>>> DELETED: %s", arsNameToDelete); 
     }
-
-    // Re-initialize the contents with NULL markers, clearing the contents.
     clearTheNames(arspHashTable);
     return 0;
 }
@@ -94,5 +89,8 @@ int main(void)
 void clearNewLineDelimiter(char* psInputWithNewline)
 {
     char* psFindTheNewLine = strchr(psInputWithNewline, '\n');
-    if (psFindTheNewLine != NULL) { psFindTheNewLine = NULL; }
+    if (psFindTheNewLine != NULL) 
+    { 
+        psFindTheNewLine = NULL;
+    }
 }
