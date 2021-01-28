29cfd4aa3328d265e6524ad5eb58606b078d4e9f
Braiden <bmxfiend1@gmail.com>
2021-01-27 23:36:04 -0500

    Improvments !

--- CircularDoublyLinkedList/weapons.c
+++ CircularDoublyLinkedList/weapons.c
@@ -2,12 +2,15 @@
  *  FILE            :   weapons.c
  *  PROJECT         :   None
  *  PROGRAMMER      :   Braiden Gole
- *  FIRST VERSION   :   2020-06-25
+ *  FIRST VERSION   :   2020-06-25 - (2021-01-27)
  *  DESCRIPTION     :   This is the implementation of a circular linked list.
  * 
  *          Note:   This circular linked list represents a weapon wheel that
  *                  you would find in a video game like Red Dead Redemption 2 where you can
  *                  cycle through weapons.
+ *          
+ *          (Improvment): Display function will now display every time instead of after the second
+ *                        entry has been made, it will now display on the first entry !
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,10 +21,10 @@
 
 int main(void)
 {   
-    struct Weapon* pHead = NULL;
-    struct Weapon* pTail = NULL;
+    struct Weapon* head = NULL;
+    struct Weapon* tail = NULL;
 
-    int iSelection = 0;
+    int selection = 0;
     char arsWeaponToWield[K100BYTES] = {""};
 
     bool bWeaponWheel = true;
@@ -35,7 +38,7 @@ int main(void)
         printf(KSELECTION4);
         printf(KSELECTION5);
         printf(KPROMPTFORSELECTION);
-        if ((fscanf(stdin, "%i", &iSelection)) == 0)
+        if ((fscanf(stdin, "%i", &selection)) == 0)
         {
             fprintf(stderr, KNOCHARACTERSALLOWED);
             fflush(stdin);
@@ -43,33 +46,33 @@ int main(void)
         printf("\n");
 
         // Associate the users selection with the proper operation.
-        switch (iSelection)
+        switch (selection)
         {
             case 1:
-                bAddWeapon(&pHead, &pTail);
+                addWeapon(&head, &tail);
                 break;
             case 2:
                 // Show the weapon wheel so the user can choose a weapon to wield.
-                showWeaponWheel(pHead);
+                showWeaponWheel(head, tail);
 
                 getchar();
                 printf(KPROMPTFORWIELDNEWWEAPON);
                 fgets(arsWeaponToWield, K100BYTES, stdin);
-                printf("\n\tWEAPON UPDATED: %s", psWieldNewWeapon(pHead, arsWeaponToWield));
+                printf("\n\tWEAPON UPDATED: %s", wieldNewWeapon(head, arsWeaponToWield));
                 break;
             case 3:
-                if ((bDeleteWeaponAtHead(&pHead, &pTail)) == false) 
+                if ((deleteWeaponAtHead(&head, &tail)) == false) 
                 { 
                     fprintf(stderr, KEMPTY);
                 }
                 printf(KDELETED);
                 break;
             case 4:
-                showWeaponWheel(pHead);
+                showWeaponWheel(head, tail);
                 break;
             case 5:
-                /** -- Free all Dynamically allocated memory -- **/
-                wipeWeaponWheel(pHead);
+                /** -- Free all Dynamicall allocated memory -- **/
+                wipeWeaponWheel(head);
                 bWeaponWheel = false;
         }
     }
